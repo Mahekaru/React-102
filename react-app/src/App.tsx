9b9501f5126042e9543161f828730e173897df0f
Mahekaru Hitokui <mahekaru@gmail.com>
2025-12-03 18:52:04 -0500

    Handle Forms

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import Message from "./Message";
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
+import Form from "./Form";
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>This is an alert message!</Alert>}
-      <button className="btn btn-primary m-2" onClick={() => setAlertVisible(true)}>Show Alert</button>
+      <Form></Form>
     </div>
   );
 }
