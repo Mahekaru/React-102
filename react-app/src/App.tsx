13311c0bb02cddc38871ef510d45a7cd17736b67
Mahekaru Hitokui <mahekaru@gmail.com>
2025-12-04 12:36:30 -0500

    state

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,10 +4,19 @@ import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 import Form from "./Form";
 function App() {
+  // Grouping related state together
+  // Avoid deeply nested states
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  const [person, setPerson] =useState({
+    firstName: '',
+    lastName: ''
+  });
+  const fullName = person.firstName + ' ' + person.lastName;
 
   return (
     <div>
-      <Form></Form>
+      {fullName}
     </div>
   );
 }
