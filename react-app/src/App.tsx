afe49752a8b528b38c423f4f73784dbfe4285b41
Mahekaru Hitokui <mahekaru@gmail.com>
2025-12-04 12:45:00 -0500

    Updating Objects

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,15 +8,26 @@ function App() {
   // Avoid deeply nested states
   // const [firstName, setFirstName] = useState("");
   // const [lastName, setLastName] = useState("");
-  const [person, setPerson] =useState({
-    firstName: '',
-    lastName: ''
-  });
-  const fullName = person.firstName + ' ' + person.lastName;
+  const[drink, setDrink] = useState({
+    title: "Americano",
+    price:5,
+  })
+
+  const handleClick = () => {
+
+    setDrink({...drink, price: 6});
+    }
+  };
+  // const [person, setPerson] =useState({
+  //   firstName: '',
+  //   lastName: ''
+  // });
+  // const fullName = person.firstName + ' ' + person.lastName;
 
   return (
     <div>
-      {fullName}
+      {/* {fullName} */}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
