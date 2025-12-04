13ba186175289fc6fb7cee1a1c69a3a3c6add100
Mahekaru Hitokui <mahekaru@gmail.com>
2025-12-04 15:17:10 -0500

    setting

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,27 +4,17 @@ import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 import Form from "./Form";
 import produce from 'immer';
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
 
 function App() {
-  // Grouping related state together
-  // Avoid deeply nested states
-const [bugs, setBugs] = useState([
-  {id:1,title:'Bug 1', fixed: false},
-  {id:2,title:'Bug 2', fixed: false},
-]);
+const [cartItems, setCartItems] = useState(['Product 1', 'Product 2']);
 
-  const handleClick = () => {
-    // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug));
-    setBugs(produce(draft => {
-      const bug = draft.find(bug => bug.id ===1);
-      if(bug) bug.fixed = true;
-    }));
-  };
 
   return (
     <div>
-      {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? "Fixed" : "Not Fixed"}</p>)}
-      <button onClick={handleClick}>Click Me</button>
+      <Navbar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 };
