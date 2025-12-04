dab2b0830534727c9ddfcddf51b82ff3e1780f11
Mahekaru Hitokui <mahekaru@gmail.com>
2025-12-04 13:31:24 -0500

    update

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -6,19 +6,13 @@ import Form from "./Form";
 function App() {
   // Grouping related state together
   // Avoid deeply nested states
-const [tags, setTags] = useState(['happy','cheerful']);
-
+const [bugs, setBugs] = useState([
+  {id:1,title:'Bug 1', fixed: false},
+  {id:2,title:'Bug 2', fixed: false},
+]);
 
   const handleClick = () => {
-    // add a new item to the array
-    setTags([...tags, 'exciting']);
-
-    // Remove 
-    setTags(tags.filter(tag => tag !== 'happy'));
-
-    // update
-    setTags(tags.map(tag => tag === 'happy' ? 'happiness' : tag));
-  
+    setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug));
   };
 
   return (
