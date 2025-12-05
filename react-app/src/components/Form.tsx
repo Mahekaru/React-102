7004c882de55d77c68ff0ead41c85ec4995bc24b
Mahekaru Hitokui <mahekaru@gmail.com>
2025-12-05 12:35:36 -0500

    form

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,26 +1,42 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({ name: '', age: '' });
 
   const handleSumit = (event: FormEvent) => {
     event.preventDefault();
+    console.log(person);
   };
 
   return (
-    <form
-      onSubmit={handleSumit}
-    >
+    <form onSubmit={handleSumit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          onChange={(Event) =>
+            setPerson({ ...person, name: Event.target.value })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+          value={person.name}
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          onChange={(Event) =>
+            setPerson({ ...person, age: (Event.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+          value={person.age}
+        />
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
